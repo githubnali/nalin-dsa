8658ca0334037ef20226e16447148a5a970321aa
githubnali <nagarajunnr341@gmail.com>
2026-02-21 00:52:19 +0530

    [DSA Prob: 08-missingNumber.js]

---
+++ arrays/08-missingNumber.js
@@ -0,0 +1,21 @@
+/**
+ * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+ */
+
+function missingNumber(nums) {
+    let n = nums.length;
+    let t_sum = n*(n+1)/2;
+    let array_sum = 0;
+
+    for (let num of nums) {
+        array_sum += num;
+    }
+
+    return t_sum - array_sum;
+}
+
+console.log(missingNumber([3,0,1]));
+
+console.log(missingNumber([0,1]));
+
+console.log(missingNumber([9,6,4,2,3,5,7,0,1]))
